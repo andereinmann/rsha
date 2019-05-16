ac1fb76d8b8ad1f463e02c9c6cf8c8ec5efd4e9c
andereinmann <ander-eric.einmann@khk.ee>
2019-05-16 13:56:21 +0300

    Lisab kasutajad koos parooliga

--- praks2/kasutajad_failist.sh
+++ praks2/kasutajad_failist.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-
-user='cat /home/user/rsha/praks2/kasutajad'
-while read user; 
-	do
-	useradd -m $user
-
-done < /home/user/rsha/praks2/kasutajad
+	if [ $# -ne 1 ]; then
+		echo "Kasutusjuhend: $0 failinimi"
+	else
+failinimi=$1
+			if [ -f $failinimi -a -r $failinimi ]; then
+			echo "Fail on korras!"
+			for rida in $(cat $failinimi)
+			do
+			kasutajanimi=$(echo "$rida" | cut -f1 -d:)
+			sh lisa_kasutaja.sh $kasutajanimi
+			echo $rida | chpasswd
+done
+		else
+		echo "Probleem failiga $failinimi"
+		fi
+fi
